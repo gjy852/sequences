2d79a2aa62b6b82ade2c339387ba6844eddd9083
Grace Yeh <gyeh@users.noreply.github.com>
2016-10-30 15:38:34 -0500

    Grace's Sequences Code (Part I)

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 group :development, :test do
   gem 'dotenv-rails'
   gem 'factory_girl_rails'
-  gem 'pry-byebug'
+  # gem 'pry-byebug'
   gem 'pry-rails'
   gem 'rspec-rails'
 end
---
+++ app/controllers/guesses_controller.rb
@@ -0,0 +1,29 @@
+class GuessesController < ApplicationController
+  def index
+    # params = {"first_number"=>"4", "second_number"=>"5", "third_number"=>"6"}
+
+    @first = params["first_number"]
+    @second = params["second_number"]
+    @third = params["third_number"]
+
+    g = Guess.new
+    g.first_num = params["first_number"]
+    g.second_num = params["second_number"]
+    g.third_num = params["third_number"]
+    g.save
+
+    @list = Guess.all
+
+    render("guesses/index.html.erb")
+  end
+
+
+
+  def answer
+    # params = {"rule"=>"Hi there"}
+
+    @user_answer = params["rule"]
+
+    render("guesses/answer.html.erb")
+  end
+end
